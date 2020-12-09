ed3131a7984db1aaac87ca1f0e352a4a2d679e70
Jack Bolles <jack@jacksonbramwell.com>
2020-12-09 15:54:57 +0000

    inline

--- src/main/kotlin/com/jb/cpwp/Player.kt
+++ src/main/kotlin/com/jb/cpwp/Player.kt
@@ -4,12 +4,10 @@ typealias Players = Set<Player>
 
 data class Player(val name:String, var hand: Set<Card> = setOf()) {
 
-    fun canPlay(table: Table) = canPlay(table.availableSlots())
+    fun canPlay(table: Table) = (hand intersect table.availableSlots()).isNotEmpty()
 
     fun cardToPlay(slots: Set<Card>): Card? { return (hand intersect slots).firstOrNull() } //todo - from brute first to ???
 
     fun play(cardToPlay: Card) { hand = hand.minus(cardToPlay) }
-
-    private fun canPlay(slots: Set<Card>): Boolean { return (hand intersect slots).isNotEmpty() }
 }
 
